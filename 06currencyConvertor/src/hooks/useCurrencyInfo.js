0da5aad841f83c74151b2713e97191e1e52b9fe9
Vanshika-Tanwar <vanshikatanwar04.bpit@gmail.com>
2024-03-13 02:16:31 +0530

    Added Currency Convertor React Project

---
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,13 @@
+//custom hooks can use built-inhooks
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({})
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`).then((res) => res.json()).then((res) => setData(res[currency]))
+    }, [currency]) 
+    return data
+}
+
+export default useCurrencyInfo;
+//finally designed a custom hook!

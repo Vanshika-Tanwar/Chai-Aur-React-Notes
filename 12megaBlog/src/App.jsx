648c94beb8641fdc2105cd65a54450dfbcd0ff62
Vanshika-Tanwar <vanshikatanwar04.bpit@gmail.com>
2024-03-18 01:07:54 +0530

    Modifications & Debugging in 12megaBlog

--- 12megaBlog/src/App.jsx
+++ 12megaBlog/src/App.jsx
@@ -4,6 +4,7 @@ import authService from './appwrite/auth'
 import {login, logout}from "./store/authSlice"
 import { Footer, Header } from './components'
 import { Outlet } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
 
 function App() {
   const [loading, setLoading] = useState(true)
